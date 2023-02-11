317f71aab9d08f227396b7a483ac438224c02e44
vynguyen <vynguyen@umich.edu>
2023-02-11 12:20:19 -0500

    clean up page.js

--- scripts/page.js
+++ scripts/page.js
@@ -1,8 +1,3 @@
-var temp = false;
-var images = ["src/IMG_1851.jpeg","src/IMG_1852.jpeg",
-"src/IMG_1853.jpeg","src/IMG_1854.jpeg","src/IMG_1856.jpeg","src/IMG_1850.jpeg"];
-var index = 0;
-
 $(document).ready(function () {
     AOS.init({
       duration: 1200,
